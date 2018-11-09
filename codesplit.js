3089d5ba2922ca70c5d40a745a9d667866f5f239
Jonathan Kuz <43478694+Jonathan204@users.noreply.github.com>
2018-11-09 10:09:53 -0600

    Add files via upload

---
+++ codesplit.js
@@ -0,0 +1,25 @@
+// Get the modal
+var cmodal = document.getElementById("cmodalID");
+
+// Get the button that opens the modal
+var cbtn = document.getElementById("codeID");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("cclose")[0];
+
+// When the user clicks the button, open the modal 
+cbtn.onclick = function() {
+    cmodal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    cmodal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        cmodal.style.display = "none";
+    }
+}
---
+++ quicksplit.js
@@ -0,0 +1,25 @@
+// Get the modal
+var qmodal = document.getElementById("qmodalID");
+
+// Get the button that opens the modal
+var qbtn = document.getElementById("quickID");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("qclose")[0];
+
+// When the user clicks the button, open the modal 
+qbtn.onclick = function() {
+    qmodal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    qmodal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == qmodal) {
+        qmodal.style.display = "none";
+    }
+}
