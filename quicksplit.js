621260be15c63b6b51fccb5976c63a93ebc74ed3
Jonathan Kuz <43478694+Jonathan204@users.noreply.github.com>
2018-11-19 19:44:39 -0600

    These are the updated horizontal prototype
    I tried to make all my code as readable as possible let me know if there is anything thats kinda hard to understand

--- quicksplit.js
+++ quicksplit.js
@@ -18,8 +18,3 @@ span.onclick = function() {
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == qmodal) {
-        qmodal.style.display = "none";
-    }
-}
