d15e152aca8d364f8cafb0c1ebbebabbec79b477
Tristan <tristanmontilla@gmail.com>
2018-11-22 11:30:20 -0600

    did some js

--- javascript/advancedsplit_handler.js
+++ javascript/advancedsplit_handler.js
@@ -1,5 +1,36 @@
 // Get the elements
 
+var itemlistcount = 0;
+var newItem = function() {
+    var itemList = document.getElementById("advSplit_BillItemList");
+    var itemListLength = itemList.getElementsByTagName('div').length;
+    var div = document.createElement("div");
+    div.className = "advSplit_ItemRow";
+
+    var child = document.createElement("input");
+    child.className = "advSplit_ItemName";
+    child.type = "text";
+    child.id = "itemName" + itemListLength;
+    child.placeholder = "Item" + itemListLength;
+    child.value = child.placeholder;
+    div.appendChild(child);
+
+    var child = document.createElement("span");
+    child.className = "money";
+    child.innerHTML = "$";
+    div.appendChild(child);
+
+    var child = document.createElement("input");
+    child.type = "number";
+    child.className = "advSplit_UserPrice";
+    child.id = "itemPrice" + itemListLength;
+    child.placeholder = "00.00";
+    child.value = child.placeholder;
+    div.appendChild(child);
+
+    itemList.appendChild(div);
+}
+
 function processGroupReq(event) {
     var group = document.getElementById("advSplit_BillGroups");
     var groupSelected = group.options[group.selectedIndex].value;
@@ -8,39 +39,60 @@ function processGroupReq(event) {
     if (group.selectedIndex > 0) {
         groupConfig.className = "";
         groupConfig.style.display = "inline;";
-        console.log("changed boi");
     }else {
         groupConfig.className = "hidden";
     }
-    console.log("yea boi");
 }
 
-var userlistcount = 0;
 function processAddUser(event) {
-    var parent = document.getElementById("advSplit_Users");
-
+    var userList = document.getElementById("advSplit_UserList");
+    var newUserList = document.getElementById("advSplit_AddUser");
+    var userListLength = userList.getElementsByTagName('div').length;
     var div = document.createElement("div");
     div.className = "advSplit_UserRow";
 
-    var field = document.createElement("input");
-    field.className = "advSplit_UserName";
-    field.id = "userName" + userlistcount;
+    var child = document.createElement("input");
+    child.className = "advSplit_UserName";
+    child.type = "text";
+    child.id = "userName" + userListLength;
+    child.value = newUserList.options[newUserList.selectedIndex].value;
+    child.readOnly = true;
+    div.appendChild(child);
 
-    var user = document.getElementById("advSplit_AddUser");
-    field.value = user.options[user.selectedIndex].value;
-    
-    field.readOnly = true;
-    div.appendChild(field);
+    var child = document.createElement("span");
+    child.className = "money";
+    child.innerHTML = "$";
+    div.appendChild(child);
 
-    field = document.createElement("input");
-    field.type = "number";
-    field.className = "userPrice";
-    //field.style = "display:inline;margin-left: 50px;";
-    field.id = "userPrice" + userlistcount;
-    field.value = "00.00";
-    field.readOnly = true;
-    div.appendChild(field);
+    var child = document.createElement("input");
+    child.type = "number";
+    child.className = "advSplit_UserPrice";
+    child.id = "userPrice" + userListLength;
+    child.value = "00.00";
+    child.readOnly = true;
+    div.appendChild(child);
 
-    parent.appendChild(div);
-    userlistcount += 1;
+    userList.appendChild(div);
+    newUserList.remove(newUserList.selectedIndex);
 }
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+//window.onclick = function(event) {
+//  if (!event.target.matches('.dropbtn')) {
+//
+//    var dropdowns = document.getElementsByClassName("dropdown-content");
+//    var i;
+//    for (i = 0; i < dropdowns.length; i++) {
+//      var openDropdown = dropdowns[i];
+//      if (openDropdown.classList.contains('show')) {
+//        openDropdown.classList.remove('show');
+//      }
+//    }
+//  }
+//}
